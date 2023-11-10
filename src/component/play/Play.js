43c435b2d832c21965c45522ef7a324de32b2066
sangharsh <sotumotghare@gmail.com>
2023-11-10 21:10:23 +0530

    commit

--- src/component/play/Play.js
+++ src/component/play/Play.js
@@ -5,31 +5,40 @@ import Asset from "../assets/Asset";
 
 const Play = () => {
   const [current, setCurrent] = useState([]);
-
+  const [audio, setAudio] = useState(null);
   const params = useParams();
 
   useEffect(() => {
     const paramsInt = parseInt(params.id);
-
     const filterParams = Asset.filter((item) => item.id === paramsInt);
-
     setCurrent(filterParams);
   }, [params.id]);
 
-  const [audio, setAudio] = useState(null)
-
   const handleImgClick = (music) => {
-    const audio = new Audio(music);
-    audio.play()
-    setAudio(audio)
-  }
-
-  const handlePause = () => {
-    if(audio){
-      audio.pause()
+    if (audio && !audio.paused) {
+      audio.pause();
+    } else {
+      const newAudio = new Audio(music);
+      newAudio.play();
+      setAudio(newAudio);
     }
-  }
+  };
 
+  // const handlePause = () => {
+  //   if (audio) {
+  //     audio.pause();
+  //   }
+  // };
+
+  useEffect(() => {
+    return () => {
+      // Clean up the audio when the component is unmounted
+      if (audio) {
+        audio.pause();
+        audio.currentTime = 0;
+      }
+    };
+  }, [audio]);
 
   console.log("current is ", current);
   return (
@@ -41,10 +50,11 @@ const Play = () => {
               <img src={i.img} alt="" />
               <div className="titles">
                 <h1 className="bg-img-line">{i.line}</h1>
-                {/* <h1 className="bg-img-title">{i.title}</h1> */}
-                <button onClick={() => handleImgClick(i.music)}>play</button>
-                <button onClick={() => handlePause()}>pause</button>
-
+                {/* <button onClick={() => handlePause()}>pause</button> */}
+                <h1 className="time-1 common" onClick={() => handleImgClick(i.music)}>
+                  Welcome
+                </h1>
+                <h1 className="time-11 common">1 min</h1>
               </div>
             </div>
           ))}
